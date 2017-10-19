544f1af7a9fd33aaf98142180b7c1cf86a6af83e
kinhoom <568483846@qq.com>
2017-10-19 16:39:08 +0800

    justLikeBaiduSearchText bug

--- handle.php
+++ handle.php
@@ -1,14 +1,81 @@
 <?php
-$value=$_POST['value'];
+$type=$_POST['type'] ? $_POST['type'] : '';
+$value=$_POST['value'] ? $_POST['value'] : '';
 $conn=mysql_connect('localhost','root','kinhoom123');
 mysql_select_db('testinnodb');
 mysql_query('set names utf8',$conn);
+
+if($value=='all'){
+	// echo 333;
+	$res=mysql_query("delete  from temp");
+	// var_dump($res);	
+	echo '';
+	exit;
+}
+
+
 $res=mysql_query("select * from temp where name like '%{$value}%' or id like '%{$value}%' or pass like '%{$value}%'");
-echo "<tr><th>id</th><th>username</th><th>password</th></tr>";
+$num=mysql_num_rows($res);
+$res1=mysql_query("select * from temp");
+$num1=mysql_num_rows($res1);
+// var_dump($num1);
+if($value=='getNum'){
+	if($num1==0){
+		echo 0;
+		exit;
+	}
+}
+if($type=='cdel'){
+	if($value){
+		$ret=mysql_query("delete from temp where id={$value}");
+		if($ret){
+			$res1=mysql_query("select * from temp");
+			$n1=mysql_num_rows($res1);
+			if($n1){
+				echo "<ul>";
+			}
+			while($ret=mysql_fetch_assoc($res1)){
+			echo "<li><a href='{$ret[url]}'>{$ret['id']}&nbsp;&nbsp;";
+			echo "{$ret['name']}&nbsp;&nbsp;";
+			echo "{$ret['pass']}</a><span id=s1 value='{$ret[id]}'>删除</span></li>";
+			}
+			if($n1){
+				echo "<div style='text-align:right;font-size:5px;color:#999;cursor:default;'><span value='all'>清除历史记录</span></div>";
+			}
+		}
+	}
+	
+	exit;
+}
+
+if($num1){
+	echo "<ul>";
+}
 while($ret=mysql_fetch_assoc($res)){
-	echo "<tr>";
-	echo "<td>{$ret['id']}</td>";
-	echo "<td>{$ret['name']}</td>";
-	echo "<td>{$ret['pass']}</td>";
-	echo "</tr>";
+	// echo $ret['id']."&nbsp;";
+	// echo $value;
+	preg_match_all("/{$value}/", $ret['id'], $resword);
+	if($resword[0][0]){
+		echo ("<li><a href='{$ret[url]}'>".str_replace($resword[0][0], "<b>{$resword[0][0]}</b>", $ret['id'])."&nbsp;&nbsp;");
+	}else{
+		echo "<li><a href='{$ret[url]}'>{$ret['id']}&nbsp;&nbsp;";
+	}
+	preg_match_all("/{$value}/", $ret['name'], $resword1);
+	if($resword1[0][0]){
+		echo ("".str_replace($resword1[0][0], "<b>{$resword1[0][0]}</b>", $ret['name'])."&nbsp;&nbsp;");
+	}else{
+		echo "{$ret['name']}&nbsp;&nbsp;";
+	}
+	preg_match_all("/{$value}/", $ret['pass'], $resword2);
+	// var_dump($res1[0][0]);
+	if($resword2[0][0]){
+		echo ("".str_replace($resword2[0][0], "<b>{$resword2[0][0]}</b>", $ret['pass'])."</a><span id=s1 value='{$ret[id]}'>删除</span></li>");
+	}else{
+		echo "{$ret['pass']}</a><span id=s1 value='{$ret[id]}'>删除</span></li>";
+	}
+}
+if($num1){
+	echo "</ul>";
+	 echo "<div style='text-align:right;font-size:5px;color:#999;cursor:default;'><span>清除历史记录</span></div>";
+
 }
--- index.php
+++ index.php
@@ -3,37 +3,146 @@
 <head>
 	<meta charset="UTF-8">
 	<title>test</title>
+	<style>
+		*{
+			margin:0px;
+			padding: 0px;
+		}
+		#search{
+			outline:none;
+			width: 280px;
+			height: 20px;
+		}
+		#d1{
+			float:left;
+			border:thin solid #ccc;
+			border-top: none;
+			background-color: #fff;
+			width:282px;
+			margin-left: 485px;
+			margin-top:0px;
+			display: none;
+		}
+		#container{
+			float:left;
+			width:284px;
+			margin-left:485.3px;
+			margin-top: 219px;
+			cursor:default;
+		}
+		#con{
+			/*background-color: #ccc;*/
+			height:1000px;
+		}
+		ul{
+			list-style-type: none;
+		}
+		li:hover{
+			background-color: #ccc;
+		}
+		a{
+			cursor:default;
+			text-decoration: none;
+			width:inherit;
+		}
+		span:hover{
+			background-color: #ccc;
+			color:#fff;
+		}
+		#s1{
+			font-size:6px;
+			float:right;
+			color:#ccc;
+		}
+		#s1:hover{
+			color:#fff;
+			text-decoration: underline;
+			cursor:default;
+		}
+	</style>
 	<script src="jquery.js" type="text/javascript"></script>
 	<script>
 		$(function(){
+			$('span[id=s1]').on('click',function(){
+				$.post('handle.php',{value:$(this).attr('value'),type:'cdel'},function(data){
+						$('#d1').html(data);
+			    });	
+			});
+			$("#search").on('mousedown',function(){
+				$.post('handle.php',{value:'getNum'},function(data){
+					if(data==0){
+						$('#d1').css('display','none');
+					}else{
+						$('#d1').css('display','block');
+					}
+					
+				});
+
+			$("#container").on('mousedown',function(){
+				$('#d1').css('display','block');
+			})
+			$("div").bind('mousedown',function(){
+				$('#d1').css('display','none');
+				$('#d1').bind('mousedown',function(){
+					$('#d1').css('display','block');
+				})
+			})
+			// $("#search").on('blur',function(){
+			// 	$('#d1').on('click',function(){
+			// 		$('#d1').css('display','block');
+			// 	})
+
+			// })
+
 			$('#search').bind('keyup',function(){
 				val=$(this).val();
 				$.post('handle.php',{value:val},function(data){
-					$('#t1').html(data);
+					if(data!=''){
+						$('#d1').css('display','block');
+						$('#d1').html(data);
+					}else{
+						$('#d1').css('display','none');
+					}
 				})
 			})
+			$('span').bind('click',function(){
+				val=$(this).attr('value');
+				$.post('handle.php',{value:val},function(data){
+					if(data!=''){
+						$('#d1').css('display','block');
+						$('#d1').html(data);
+					}else{
+						$('#d1').css('display','none');
+					}
+				})
+			})
+		});
 		});
 	</script>
 </head>
 <body>
-	<input type="text" id='search' name='search'><span>查找</span>
-	<div class="">
+	<div id="container">
+	<input type="text" id='search' name='search'>
+	</div>
 	<?php
 		$conn=mysql_connect('localhost','root','kinhoom123');
 		mysql_select_db('testinnodb');
 		mysql_query('set names utf8',$conn);
 		$res=mysql_query('select * from temp;');
-		echo "<table id='t1' border=1>";
-		echo "<tr><th>id</th><th>username</th><th>password</th></tr>";
+		$num=mysql_num_rows($res);
+		echo "<div id='d1'>";
+		echo "<ul>";
 		while($ret=mysql_fetch_assoc($res)){
-			echo "<tr>";
-			echo "<td>{$ret['id']}</td>";
-			echo "<td>{$ret['name']}</td>";
-			echo "<td>{$ret['pass']}</td>";
-			echo "</tr>";
+			echo "<li><a href='{$ret[url]}'>{$ret['id']}&nbsp;&nbsp;";
+			echo "{$ret['name']}&nbsp;&nbsp;";
+			echo "{$ret['pass']}</a><span id=s1 value='{$ret[id]}'>删除</span></li>";
 		}
-		echo "</table>";
+		if($num){
+			echo "<div style='text-align:right;font-size:5px;color:#999;cursor:default;'><span value='all'>清除历史记录</span></div>";
+		}
+		echo "</ul>";
+		echo "</div>";
 	?>
-	</div>
+	<div id="con"></div>
 </body>
 </html>
